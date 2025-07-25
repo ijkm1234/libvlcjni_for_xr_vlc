f9e4284ad0c296d56414ef761b085b09738fcae9
Steve Lhomme <slhomme@matroska.org>
2025-07-25 09:16:39 +0200

    buildsystem: update VLC to support NDK 27+
    So we can build the 64-bit variants with newer SDKs.

--- buildsystem/get-vlc.sh
+++ buildsystem/get-vlc.sh
@@ -43,7 +43,7 @@ check_patch_is_applied()
     fi
 }
 
-VLC_TESTED_HASH=8a7f54566a01a9990a60473b6a2417481a074cce
+VLC_TESTED_HASH=7ad9339a5fbd0976159cea3e6c568ff0b6835dcc
 VLC_REPOSITORY=https://code.videolan.org/videolan/vlc.git
 VLC_BRANCH=3.0.x
 
